d1147006db466c5c6f8b8ea736a9302e82fe8a4b
aditi2906 <aditi19csu011@ncuindia.edu>
2023-01-29 01:05:21 +0530

    resolving merging issues

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,8 @@ import "./index.css";
 import App from "./App";
 import { NextUIProvider } from "@nextui-org/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<NextUIProvider>{/* <App /> */}</NextUIProvider>);
+root.render(
+  <NextUIProvider>
+   
+  </NextUIProvider>
+);
